bc821cbe5ca10a27bc39ef4926b6429c8404c98e
EricNohara <ernohara@bu.edu>
2023-09-11 18:15:21 -0400

    added working part 3 code

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,8 +1,10 @@
-let rec getDigit(i: int): int = 
-
-let rec intLength(i: int): int = 
-  if i < 10 then i else 1 + intLength(i / 10)   (*if at the ones digit, return *)
+let rec getDigit(i: int)(i0: int): char =
+  if i = 0 then Char.chr (48+(i0 mod 10)) else getDigit(i-1)(i0/10)
+  
+let rec intLength(i0: int): int = 
+  if i0 < 10 then 1 else 1 + intLength(i0 / 10)   (*if at the ones digit, return *)
     
 
 let int2str(i0: int): string = 
-  String.init(intLength(i0)) (fun i -> getDigit(i))
+  let length = intLength(i0) in
+  String.init (length) (fun (i) -> getDigit(length-i-1)(i0))
