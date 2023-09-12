c6acd27fef09ce94c32397931e0c1aaad84ea2b8
EricNohara <ernohara@bu.edu>
2023-09-11 21:45:28 -0400

    added comments for assign0-4

--- assigns/assign0/MySolution/assign0-4.ml
+++ assigns/assign0/MySolution/assign0-4.ml
@@ -1,14 +1,9 @@
-(* Please implement a function that converts a given
-string to an integer:
-fun str2int(cs: string): int
-In particular, it is expected that str2int(int2str(x)) = x
-for natural numbers x.
-(You can assume that the given string is a sequence of digits)
-(And the empty sequence represents the integer 0) *)
+(*helper function to accumulate the int from the digits of the string*)
 let rec accumulate (i: int) (str: string): int =
-  if i < 0 then 0 else 
-    let digitChar = String.get(str)(i) in let digitValue = Char.code(digitChar) - (Char.code ('0')) in digitValue + 10 * accumulate(i-1)(str)
+  if i < 0 then 0 else  (*Base case: when the current index points outside the string, return 0*)
+    let digitChar = String.get(str)(i) in let digitValue = Char.code(digitChar) - (Char.code ('0')) in digitValue + 10 * accumulate(i-1)(str)   (*get the correct char at the correct position in the string and convert it to an int, then add it to the total*)
 
+(*wrapper function that creates a string by calling the helper accumulate function with the correct inputs*)
 let str2int(cs: string): int = 
   let length = String.length(cs) in 
     accumulate(length-1)(cs)
