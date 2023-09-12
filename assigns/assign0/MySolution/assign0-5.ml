3e9332fc0fdccb9ffcdcb6c36c0a2890ae6c21b7
EricNohara <ernohara@bu.edu>
2023-09-11 22:02:18 -0400

    added comments for assign0-5

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -1,13 +1,8 @@
-(* Assign0-5: 10 points
-Please implement a function that returns the reverse of
-a given string:
-fun stringrev(cs: string): string
-Note that you are not allowed to use string concatenation
-or your solution is disqualified. *)
-
+(*helper function used in the anonymous function to reverse the string one character at a time*)
 let rec reverseString (i: int) (str: string): char = 
-  String.get(str)(i)
+  String.get(str)(i)    (*return the character at the corresponding index value of the string*)
 
+(*wrapper function to create a new string of length cs.length and with the correct contents of the reversed string*)
 let stringrev(cs: string): string = 
   let length = String.length (cs) in 
-  String.init (length) (fun (i) -> reverseString(length-i-1)(cs))
+  String.init (length) (fun (i) -> reverseString(length-i-1)(cs)) (*length-i-1 will give the corresponding index of the correct char needed to be inserted into the string such that the string reverses*)
