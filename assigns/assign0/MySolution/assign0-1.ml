00fc2bea6a8d9fbd6a468a20d25c518aab8ad357
EricNohara <ernohara@bu.edu>
2023-09-11 22:11:52 -0400

    added comments for assign0-1

--- assigns/assign0/MySolution/assign0-1.ml
+++ assigns/assign0/MySolution/assign0-1.ml
@@ -1,7 +1,9 @@
+(*recursive function that finds the factorial of a given int*)
 let rec fact(x: int): int =
   if x > 0 then x * fact(x-1) else 1
   
+(*recursive function that finds when x overflows when plugged into the factorial function*)
 let rec findoverflow(x: int): int =
-  if fact(x) = 0 then  x else  findoverflow(x + 1)
+  if fact(x) = 0 then x else  findoverflow(x + 1)
 
 let myans = findoverflow(0);;
