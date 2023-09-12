f790b9ee21186dab56f652a441f34793b841605b
EricNohara <ernohara@bu.edu>
2023-09-11 21:59:04 -0400

    added implementation of assign0-5

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -5,4 +5,9 @@ fun stringrev(cs: string): string
 Note that you are not allowed to use string concatenation
 or your solution is disqualified. *)
 
+let rec reverseString (i: int) (str: string): char = 
+  String.get(str)(i)
+
 let stringrev(cs: string): string = 
+  let length = String.length (cs) in 
+  String.init (length) (fun (i) -> reverseString(length-i-1)(cs))
