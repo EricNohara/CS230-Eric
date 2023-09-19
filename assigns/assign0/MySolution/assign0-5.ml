9e667af708952222db51677a6cf920b083613d40
EricNohara <ernohara@bu.edu>
2023-09-19 14:39:18 -0400

    fixes to assign0

--- assigns/assign0/MySolution/assign0-5.ml
+++ assigns/assign0/MySolution/assign0-5.ml
@@ -1,8 +1,10 @@
+#use "./../../../classlib/OCaml/MyOCaml.ml"
+
 (*helper function used in the anonymous function to reverse the string one character at a time*)
 let rec reverseString (i: int) (str: string): char = 
-  String.get(str)(i)    (*return the character at the corresponding index value of the string*)
+  string_get_at str i    (*return the character at the corresponding index value of the string*)
 
 (*wrapper function to create a new string of length cs.length and with the correct contents of the reversed string*)
 let stringrev(cs: string): string = 
-  let length = String.length (cs) in 
-  String.init (length) (fun (i) -> reverseString(length-i-1)(cs)) (*length-i-1 will give the corresponding index of the correct char needed to be inserted into the string such that the string reverses*)
+  let length = string_length cs in 
+  string_init length (fun (i) -> reverseString(length-i-1)(cs)) (*length-i-1 will give the corresponding index of the correct char needed to be inserted into the string such that the string reverses*)
