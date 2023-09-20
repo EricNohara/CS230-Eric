14cbd2497b5794339e8bd376c4df497206d4ea73
EricNohara <ernohara@bu.edu>
2023-09-20 14:56:36 -0400

    made function tail recursive

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,12 +1,14 @@
 #use "./../assign0.ml";;
    
+(*helper function used in the anonymous function that will find the correct digit from the int and list it as a char*)
+let rec getDigit(i: int)(i0: int): char =
+  if i = 0 then chr (48+(i0 mod 10)) else getDigit(i-1)(i0/10)   (*when i = 0, return the char of the current ones digit, else call the function again with one less digit in i0*)
+
+(*helper function that will find the correct length of the string based off of the digits of the inputted int*)
+let rec intLength(i0: int) (numdigits: int): int = 
+  if i0 < 10 then numdigits else intLength(i0 / 10) (numdigits+1)   (*if at the ones digit, return *) 
+
 (*wrapper function that creates a new string with the correct length and contains the correct digits from the int*)
 let int2str(i0: int): string = 
-  (*helper function used in the anonymous function that will find the correct digit from the int and list it as a char*)
-  let rec getDigit(i: int)(i0: int): char =
-  if i = 0 then chr (48+(i0 mod 10)) else getDigit(i-1)(i0/10)   (*when i = 0, return the char of the current ones digit, else call the function again with one less digit in i0*) in
-  (*helper function that will find the correct length of the string based off of the digits of the inputted int*)
-  let rec intLength(i0: int): int = 
-  if i0 < 10 then 1 else 1 + intLength(i0 / 10)   (*if at the ones digit, return *) in 
-  let length = intLength(i0) in
+  let length = intLength i0 1 in
   string_init (length) (fun (i) -> getDigit(length-i-1)(i0))    (*initialize a string of the length of i0 and the correct digits in order*)
