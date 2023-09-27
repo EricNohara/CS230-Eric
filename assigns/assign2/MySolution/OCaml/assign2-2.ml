56b21aa96c6512c9fb606aa335f5c1f124db38b2
EricNohara <ernohara@bu.edu>
2023-09-27 10:39:49 -0400

    fixed bug in indexing in assign2-2

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -32,7 +32,7 @@ let rec mylist_get_at (xs: 'a mylist) (i0: int): 'a =
     match xs with
     | MyNil -> mylist_subscript_exn()
     | MyCons (x1, xs) -> if i0 = 0 then x1 else mylist_get_at xs (i0-1) 
-    | MySnoc (xs, x1) -> if i0 = (length-1) then x1 else mylist_get_at xs (i0+1)
+    | MySnoc (xs, x1) -> if i0 = (length-1) then x1 else mylist_get_at xs (i0)
     | MyReverse xs -> mylist_get_at xs (length-i0-1)
     | MyAppend2 (xs1, xs2) -> 
       let len1 = mylist_length xs1 in
